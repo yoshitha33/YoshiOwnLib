64a6b89781f39aad27f2bf9a94b99dd1f5531547
yoshitha33 <yoshithanunna77@gmail.com>
2025-05-24 16:38:54 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name ="YoshiOwnLib",
+    version=0.1,
+    author="Yoshitha Nunna",
+    author_email="yoshithanunna77@gmail.com",
+    description="A Python package for data analysis and visualization using Pandas, NumPy, Matplotlib, and Seaborn.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas>=1.0",
+        "numpy>=1.18",
+        "matplotlib>=3.0",
+        "seaborn>=0.10"
+    ],
+)
